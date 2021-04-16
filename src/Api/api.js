b571f8ba79a75810ede4c3769a4f55b4313d2e83
Antony Rafael <depaulacostaantony@gmail.com>
2021-04-16 16:34:45 -0300

    Comment Post

--- src/Api/api.js
+++ src/Api/api.js
@@ -40,16 +40,16 @@ export function USER_POST(body) {
   return {
     url: API_URL + "/api/user",
     options: {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
+      body: JSON.stringify(body),
+    },
   };
 }
 
-export function PHOTO_POST(formData,token) {
+export function PHOTO_POST(formData, token) {
   return {
     url: API_URL + "/api/photo",
     options: {
@@ -62,22 +62,35 @@ export function PHOTO_POST(formData,token) {
   };
 }
 
-export function PHOTOS_GET({page, total, user}) {
+export function PHOTOS_GET({ page, total, user }) {
   return {
-    url:  `${API_URL}/api/photo/?_page=${page}&_total=${total}&_user=${user}`,
+    url: `${API_URL}/api/photo/?_page=${page}&_total=${total}&_user=${user}`,
     options: {
       method: "GET",
-      cache: 'no-store'
+      cache: "no-store",
     },
   };
 }
 
 export function PHOTO_GET(id) {
   return {
-    url:  `${API_URL}/api/photo/${id}`,
+    url: `${API_URL}/api/photo/${id}`,
     options: {
       method: "GET",
     },
   };
 }
 
+export function COMMENT_POST(id, body) {
+  return {
+    url: `${API_URL}/api/comment/${id}`,
+    options: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + window.localStorage.getItem("token"),
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
