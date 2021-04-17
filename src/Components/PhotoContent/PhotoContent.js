ce797de374ba0881cba39c65f0f098f7f368835e
Antony Rafael <depaulacostaantony@gmail.com>
2021-04-17 15:36:57 -0300

    Photo delete

--- src/Components/PhotoContent/PhotoContent.js
+++ src/Components/PhotoContent/PhotoContent.js
@@ -1,33 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import PhotoComments from './PhotoComments';
-import styles from './PhotoContent.module.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../UserContext";
+import PhotoComments from "./PhotoComments";
+import styles from "./PhotoContent.module.css";
+import PhotoDelete from "./PhotoDelete";
 
-const PhotoContent = ({data}) => {
+const PhotoContent = ({ data }) => {
+  const user = React.useContext(UserContext);
+  const { photo, comments } = data;
+  console.log(data);
+  return (
+    <div className={styles.photo}>
+      <div className={styles.img}>
+        <img src={photo.src} alt={photo.title} />
+      </div>
+      <div className={styles.details}>
+        <div>
+          <p className={styles.author}>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
 
-    const {photo, comments} = data;
-    console.log(data)
-    return (
-        <div className={styles.photo}>
-            <div className={styles.img}>
-                <img src={photo.src} alt={photo.title} />
-            </div>
-            <div className={styles.details}>
-                <div>
-                    <p className={styles.author}>
-                        <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
-                        <span className={styles.visualizacoes}>{photo.acessos}</span>
-                    </p>
-                    <h1 className="title"><Link to={`/foto/${photo.id}`}>{photo.title}</Link></h1>
-                    <ul className={styles.attributes}>
-                        <li>{photo.peso} kg</li>
-                        <li>{photo.idade} anos</li>
-                    </ul>
-                </div>
-            </div>
-            <PhotoComments id={photo.id} comments={comments} />
+            <span className={styles.visualizacoes}>{photo.acessos}</span>
+          </p>
+          <h1 className="title">
+            <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} kg</li>
+            <li>{photo.idade} anos</li>
+          </ul>
         </div>
-    )
-}
+      </div>
+      <PhotoComments id={photo.id} comments={comments} />
+    </div>
+  );
+};
 
-export default PhotoContent
+export default PhotoContent;
